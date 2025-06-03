389b223c309884358a1191c2ea7c191c95adc5dd
Brent McManus <brent.mcmanus@astronomer.io>
2025-06-03 08:33:49 -0700

    improve lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,43 +5,29 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'ayu_dark',
+                theme = 'iceberg_dark',
                 component_separators = { left = ' ', right = ' '},
                 section_separators = { left = ' ', right = ' '},
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
-                },
+                disabled_filetypes = { },
                 ignore_focus = {},
                 always_divide_middle = true,
-                always_show_tabline = true,
+                always_show_tabline = false,
                 globalstatus = false,
-                refresh = {
-                    statusline = 100,
-                    tabline = 100,
-                    winbar = 100,
-                }
+                refresh = { statusline = 100, tabline = 100, winbar = 100 }
             },
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
-                lualine_y = {},
-                lualine_z = {}
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {
+                    function()
+                        -- expands to relative path, replace . with p for absolute
+                        return vim.fn.expand('%:.')
+                    end
+                },
+                lualine_z = {'filetype'}
+            }
         }
     end
 }
