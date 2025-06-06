eb2cc819c91044c12a268c9698ee2e052125afc9
bamcmanus <brent.mcmanus@me.com>
2025-06-05 20:56:13 -0700

    add gitignore for lazy-lock.json and fix smear interference with code actions

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -5,6 +5,9 @@ return {
     -- Smear cursor when switching buffers or windows.
     smear_between_buffers = true,
 
+    -- must be set to false to prevent interference with lsp code actions
+    smear_to_cmd = false,
+
     -- Smear cursor when moving within line or to neighbor lines.
     -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
     smear_between_neighbor_lines = true,
@@ -18,6 +21,6 @@ return {
 
     -- Smear cursor in insert mode.
     -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-    smear_insert_mode = true,
+    smear_insert_mode = false,
   },
 }
